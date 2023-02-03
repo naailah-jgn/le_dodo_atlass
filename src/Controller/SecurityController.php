cb185a2ca275400d2150e460264ce1778090fc66
Naailah JUGON <jugonnaailah@gmail.com>
2023-02-03 01:39:18 +0100

    bug sur branch new_user, creation de user_new, creation formulaire login et pwd hasher

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\LoginType;
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    #[Route('/connection', name: 'security.login', methods: ['GET', 'POST'])]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+       
+        return $this->render('security/login.html.twig', [
+            'last_username' => $authenticationUtils->getLastUsername(),
+            'error' => $authenticationUtils->getLastAuthenticationError()
+        ]);
+    }
+
+    #[Route('/deconnexion', name: 'security.logout')]
+    public function logout()
+    {
+
+    }
+}
